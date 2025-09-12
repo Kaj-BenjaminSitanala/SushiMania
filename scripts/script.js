5da31047e73d23e19575a052c37fef176c0d456d
Kaj-BenjaminSitanala <kaj.benjamin.sitanala@gmail.com>
2025-09-12 18:43:47 +0200

    Slidemnu even terug

--- scripts/script.js
+++ scripts/script.js
@@ -11,21 +11,21 @@ const hamburgerIcon = document.getElementById('hamburger-icon');
   });
   
 // SlideMenu autoslide
-//const slides = document.querySelectorAll('.slide');
-//let current = 0;
+const slides = document.querySelectorAll('.slide');
+let current = 0;
 
-//function showSlide(index) {
-//  slides.forEach(slide => slide.classList.remove('active'));
-// slides[index].classList.add('active');
-//}
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove('active'));
+ slides[index].classList.add('active');
+}
 
-//function nextSlide() {
-//  current = (current + 1) % slides.length;
-//  showSlide(current);
-//}
-//showSlide(current);
+function nextSlide() {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}
+showSlide(current);
 
-//setInterval(nextSlide, 7000);
+setInterval(nextSlide, 7000);
 
 
 
