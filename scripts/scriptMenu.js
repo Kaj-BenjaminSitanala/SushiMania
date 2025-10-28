e910f85551cd1b62a96aabee571e5e94deb442a4
Kaj-BenjaminSitanala <kaj.benjamin.sitanala@gmail.com>
2025-10-28 17:03:46 +0100

    boom

--- scripts/scriptMenu.js
+++ scripts/scriptMenu.js
@@ -1,6 +1,28 @@
-const openMenu = document.querySelector("main section button");
-  const menuList = document.querySelector("main section form");
+// https://css-tricks.com/working-with-javascript-media-queries/
 
-  openMenu.addEventListener('click', () => {
-    menuList.classList.toggle('open');
-  });
+var mediaQuery = window.matchMedia("(width = 48rem)")
+// let myInputs = document.querySelector("form label input")
+
+if (mediaQuery.matches) {
+  // https://www.w3schools.com/jsreF/prop_checkbox_checked.asp
+  unCheck();
+}
+
+function unCheck() {
+  var myInput = document.querySelector("input");
+    myInput.checked = false; //test
+    document.querySelector("select option:checked").checked = false; //andere test
+}
+
+// https://modx.com/blog/checking-and-unchecking-all-checkboxes-in-a-form
+// function checkAll(checkEm) {
+//     var cbs = document.querySelector("input");
+
+//     for (var i = 0; i < cbs.length; i++) {
+//         if (cbs[i].type == 'checkbox') {
+//             if (cbs[i].name == 'my_form_tasks[]') {
+//                 cbs[i].checked = checkEm;
+//             }
+//         }
+//     }
+// }
